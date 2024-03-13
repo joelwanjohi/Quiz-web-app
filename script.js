83ed1c51aa1fe2bdf2e9d836bb663c7c3e76989c
Joel Wanjohi <joelwanjohi207@gmail.com>
2024-03-13 21:37:55 +0300

    ggsgd

---
+++ script.js
@@ -0,0 +1,15 @@
+let questions = [
+   
+    // you can uncomment the below codes and make duplicate as more as you want to add question
+    // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+      {
+      numb: 1,
+      question: "What domain deals with microcontrollers?",
+      answer: "Embedded Systems Development",
+      options: [
+        "Back-end Development",
+        "Data Science",
+        "Embedded Systems Development", 
+        "API Development "
+      ]
+    },
